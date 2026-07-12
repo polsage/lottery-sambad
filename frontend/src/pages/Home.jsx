961ac133f16fa452f123b22c446e9cb6607fea22
polsage <prabha.shyamu@gmail.com>
2026-07-12 17:28:22 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+
+    return (
+
+        <div className="container">
+
+            <h1>Lottery Sambad</h1>
+
+            <h2>Today's Result</h2>
+
+            <p>Welcome to Lottery Sambad.</p>
+
+        </div>
+
+    )
+
+}
