c5ba09609f81e2520f535ba33cbe5b63b6298cfc
polsage <prabha.shyamu@gmail.com>
2026-07-12 17:30:32 +0530

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+export default function NotFound(){
+
+    return(
+
+        <div className="container">
+
+            <h1>404</h1>
+
+            <p>Page Not Found</p>
+
+        </div>
+
+    )
+
+}
