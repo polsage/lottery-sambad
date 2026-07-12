e6f3fa0a2ea0b867f406a0e78d7d57912a56a8e2
polsage <prabha.shyamu@gmail.com>
2026-07-12 17:34:47 +0530

    Create Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import "./Footer.css";
+
+export default function Footer(){
+
+return(
+
+<footer className="footer">
+
+<div className="container">
+
+<p>
+
+© 2026 Lottery Sambad. All Rights Reserved.
+
+</p>
+
+</div>
+
+</footer>
+
+)
+
+}
