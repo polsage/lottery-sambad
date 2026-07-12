25e519f2807b76c331d2e6ace3c394bbfeca924c
polsage <prabha.shyamu@gmail.com>
2026-07-12 17:46:34 +0530

    Create results.js

---
+++ backend/src/data/results.js
@@ -0,0 +1,26 @@
+const results = [
+
+    {
+        id:1,
+        draw:"Morning",
+        time:"10:00 AM",
+        number:"45821"
+    },
+
+    {
+        id:2,
+        draw:"Day",
+        time:"01:00 PM",
+        number:"90124"
+    },
+
+    {
+        id:3,
+        draw:"Evening",
+        time:"06:00 PM",
+        number:"67281"
+    }
+
+];
+
+export default results;
