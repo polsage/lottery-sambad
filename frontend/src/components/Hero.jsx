17ab1992451cb9e37b67268cc10e73eb5434af17
polsage <prabha.shyamu@gmail.com>
2026-07-12 17:37:23 +0530

    Create Hero.jsx

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container hero-content">
+        <h1>Lottery Sambad Results</h1>
+
+        <p>
+          Fast • Accurate • Daily Lottery Results
+        </p>
+
+        <button>Today's Result</button>
+      </div>
+    </section>
+  );
+}
